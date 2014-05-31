d383c6e412fcb3da867b048454abbe93ff7df033
Yen Tran <tran.yentran@gmail.com>
2014-05-30 22:43:38 -0700

    added psuedocode to code combat

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,16 +1,108 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge [by myself, with:] 
+I worked on this challenge by myself
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+// RESCUE MISSION
+for (int 1=0; i<2; i++)
+{ return 
+// move down x2
+// move right 1x
+// move up x2
+// move right x2
+// move down 1x
+// ATTACK!
+}
+
+// GRAB THE MUSHROOM
+// move up 1x
+// move right 1x
+// move left 1x
+// move up 1x 
+// ATTACK!
+
+// DRINK ME
+// move right 1x
+// Attack
+// move right 1x
+// move down 1x
+// move up 1x
+// move right 1x
+// Attack 
+// move right 1x
+
+// TAUNT THE GUARDS
+// move right 1x
+// bust DownDoor 1x
+// move right 1x
+// say something 1x
+// move left 2x
+// Attack
+// move right 1x
+// say something 1x
+// move right 2x
+// move up 1x
+// move right 1x
+// say something 1x
+// move down 1x
+// move right 1x
+// move up 1x
+// move right 2x
+
+
+// IT'S A TRAP
+// move down 2x
+// say something 1x
+// move up 1x
+// say something 1x
+// move up 1x
+
+
+// BREAK THE PRISON
+// If name is 'William'
+// 	breakdown door (return true)
+// If name is 'Krogg'
+// 	skip door (return false)
+// If name is 'Lucas'
+// 	breakdown door (return true)
+// If name is 'Brack'
+// 	skip door (return false)
+// If name is 'Marcus'
+// 	breakdown door (return true)
+// If name is 'Gordon'
+// 	breakdown door (return true)
+
+
+// TAUNT 
+// say something (taunt) 4x
+
+
+// COWARDLY TAUNT
+// move to location (50, 16) 
+// move to location (63, 20)
+// say something 1x 
+// move to location (70, 10)
+
+
+// COMMANDING FOLLOWERS
+// move to location (49, 66)
+// move to location (60, 63)
+// move to lcoation (75, 63)
+// say something 1x
+// move to location (74, 42)
+// say something 1x
+
  
- 
- 
- 
- 
- 
- 
+// MOBILE ARTILLERY
+// move to location (30, 26) 
+// aim and attack at this location (46, 5) 
+// move to location (46, 40)
+// aim and attack at this location(69, 56)
+// aim and attack at this location(57, 48)
+// aim and attack at this location(50, 68)
+// aim and attack at this location(48, 49)
  
 // Reflection:
 // Write your reflection here.
