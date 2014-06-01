a2db6a62a103f462e4051570afa41748797491b8
Yen Tran <tran.yentran@gmail.com>
2014-05-31 20:02:21 -0700

    added refelction

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -12,13 +12,31 @@ var members = ["John", "Yen", "Whiskey", "Mary"]
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
+// What parts of your strategy worked? What problems did you face?
+// My strategy was to run the test in node. Needed to ask a fellow 
+// classmate to understand how to run Node in the first place. Once 
+// was able to start it and run a couple of errors, I figured it out.
 // 
+// What questions did you have while coding? What resources did you find to help you answer them?
+//Just how to run the test in the first place. Was able to reach out to 
+//a fellow classmate for help. Also had to refer to Js tutorials to setup
+//the Js code.
+//
+//Did you learn any new skills or tricks?
+//Tons of Js and Node. Although I feel like I need to go and find additional
+//Js tutorials and exercises to cement my learning.
+//
+//How confident are you with each of the Learning Competencies?
+//Unfortunately I'm not feeling confident on many of the JavaScript
+//stuff. Hopefully practice will cement it.
+//
+//Which parts of the challenge did you enjoy?
+//Completing the test and going over the JS labs
+//
+//Which parts of the challenge did you find tedious?
+//Understanding how to run a TTD. I wish there was some intial 
+//step-by-step instructions on how to set it up.
 // 
-// 
-// 
-// 
-
-
 // __________________________________________
 // Test Code:  Do not alter code below this line.
 
