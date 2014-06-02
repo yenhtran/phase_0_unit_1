b4039a44274e5e1e03f2006d8fead3a7e3f441cc
Yen Tran <tran.yentran@gmail.com>
2014-06-01 20:54:51 -0700

    added solo challenge and gps1.2

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -2,25 +2,58 @@
 
 // Your mission description:
 // 
+// The mission is that the knight Tharin needs to escape
+// the castle. He needs to steal a horse in the stable 
+// across the field. However, in order to get the horse
+// to go with him, he needs to pick up the apple that is
+// located in the middle of the field. Then give the apple
+// to the horse.
+//
 // 
 // 
 
 // Pseudocode
-// 
-// 
+// Move up 2x
+// Move right 2x
+// Pick up apple
+// Move right 2x
+// Move Up 1x
+// Present apple
+// Say "Here horsey- I have a treat for you!"
+// Give apple to horse
+// While distracted, get on horse
+// Ride away
 // 
 // 
 // 
 
 // Initial Code
 
-
-
-
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.pickApple();
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.showApple();
+this.say("Here horsey- I have a treat for you!");
+this.giveApple();
+this.getonHorse();
+this.rideaway();
 
 
 // Refactored Code
 
+var moveUp = function() {
+	[INSERT CODE BLOCK]
+}
+
+var moveRight = function() {
+	[INSERT CODE BLOCK]
+}
+
+var 
 
 
 
--- week_3/9_gps1_2/our_solution.js
+++ week_3/9_gps1_2/our_solution.js
@@ -1,24 +1,51 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Max Lock
+//  2. Yen Tran
 
 
 // 0. "YOU SIGNED... WHO?!"
 
+var angelinajolie = {
+	name: "Angelina Jolie",
+	age: 38,
+	quote: "I'm badass!"
+}
 
-
+var bradpitt = {
+    name: "Brad Pitt",
+    age: 50,
+    quote: "You don't talk about fight club."
+}
 
 // 1. "Here they Come!"
 
+var adamSandler = {
+    name: "Adam Sandler",
+    age: 47,
+    quote: "That's your home! Are you too good for your home?!"
+}
 
-
+var  kirstenBell = {
+    name: "Kristen Bell",
+    age: 33,
+    quote: "Do you wanna build a snowman?"
+}
+var jimCarrey = {
+    name: "Jim Carrey",
+    age: 52,
+    quote: "...So you're telling me there's a chance? YEAH!"
+}
 
 
 // 2. "TIME IS MONEY!"
 
-//YOUR CODE HERE!
+var Client = function(name, age, quote) {
+  this.name = name;
+  this.age = age;
+  this.quote = quote;
+}
 
 var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
 shooterMcGavin.constructor === Client;
@@ -31,15 +58,40 @@ shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to wh
 
 // 3. "SHOW 'EM OFF!"
 
+var adamSandler = new Client("Adam Sandler", 47, "That's your home! Are you too good for your home?!");
+var kirstenBell = new Client("Kristen Bell", 33, "Do you wanna build a snowman?");
+var jimCarry = new Client("Jim Carry", 52, "...So you're telling me there's a chance? YEAH!");
 
-
+var Client = function Client(name, age, quote) {
+  this.name = name;
+  this.age = age;
+  this.quote = quote;
+  this.showQuote = function () {
+      console.log("My client" + name + ", who is" + age + " says" + quote + ".");
+  }
+}
 
 
 // 4. "But wait, there's more!"
 
+var Client = function Client(name, age, quote) {
+  this.name = name;
+  this.age = age;
+  this.quote = quote;
+  this.showQuote = function() {
+  console.log(quote);
+  }
+}
 
 
 // 5.  ** BONUS **
 
 
 //  6.  Your Reflection:
+//
+// I came into this session thinking that I had somewhat of an decent
+// understanding of Js, but came out feeling lost. I had gone through
+// all the Js labs, but was stumped when we came across the constructor
+// and didn't know what to do. It sounds like the people who succeeded 
+// in this challenge were the ones who did the Js Codeacademy before hand.
+// I will likely try and squeeze in time to do it. 
